1891464c843351df5922d7077b71f88553f0410a
Suraj <surajpatel527@gmail.com>
2019-09-23 21:41:12 -0400

    Changelog: Added arrow functions, some comments. TODO: Add more arrow functions, and fix the database bug and make the database scalable

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -30,7 +30,7 @@ function makeTable() {
     });
 }
 
-function promptSupervisor() {
+promptSupervisor = () => {
     inquirer
         .prompt([
             {
@@ -88,11 +88,11 @@ function addDepartment() {
         });
 }
 
-function viewSales() {
+viewSales = () => {
     // departments table, calculates a total_profit column
     connection.query(
         "SELECT departProd.department_id, departProd.department_name, departProd.over_head_costs, SUM(departProd.product_sales) as product_sales, (SUM(departProd.product_sales) - departProd.over_head_costs) as total_profit FROM (SELECT departments.department_id, departments.department_name, departments.over_head_costs, IFNULL(products.product_sales, 0) as product_sales FROM products RIGHT JOIN departments ON products.department_name = departments.department_name) as departProd GROUP BY department_id",
-        function (err, res) {
+        (err, res) => {
             console.table(res);
             promptSupervisor();
         }
